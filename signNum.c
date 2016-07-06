fe58dd20e8a657ae76e45c688d313bb9a02f5a5d
NivethidhaaP <nivi.dreamzone@gmail.com>
2016-07-06 14:39:29 +0530

    Create signNum.c

---
+++ signNum.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+Void main()
+{
+int n;
+printf("Enter Number:");
+scanf("%d",&n);
+if(n>0)
+{
+printf("Positive");
+}
+else if(n<0)
+{
+printf("Negative");
+}
+else
+{
+printf("Zero");
+}
+getch();
+}
