3116345cdab09236239aeb442171b3a349e157bf
NivethidhaaP <nivi.dreamzone@gmail.com>
2016-09-28 18:49:44 +0530

    Create sumNatural.java

---
+++ sumNatural.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class Natural{
+public static void main(String[] args){
+ int n,i,sum=0;
+ Scanner sc=new Scanner(System.in);
+ n=sc.nextInt();
+ for(i=1;i<=n;i++){
+ sum+=i;
+ }
+ System.out.println(sum);
+ }
+ }
